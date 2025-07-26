3ea1e27cbc97fd8a426c3308a07a9b6ca996576a
youneedthistool <youneedthistools@gmail.com>
2025-07-26 10:14:26 -0300

    Update index.js via planilha

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
+
 const redirects = {
-  "/oi": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta-20"
+  "/oi": "https://www.amazon.com/dp/B0D1YD7R2C?tag=youtube0178d-20"
 };
 
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
     const target = redirects[url.pathname];
-
     if (target) {
       return Response.redirect(target, 301);
     }
-
     return new Response("Not found", { status: 404 });
   },
 };
