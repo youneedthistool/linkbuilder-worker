a63fa0005b67c66b054cf3b24a06d7fc7421efa9
youneedthistool <youneedthistools@gmail.com>
2025-07-29 13:18:54 -0300

    Update redirects with ASIN + Tracking ID

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const redirects = {
-
+  "/basics-mesh-pen-holder-and-organizer-black-tiktok": {"asin":"B08VP72ZNS","trackingId":"TikTok"},
+  "/jean-lucka-instagram": {"asin":"B08VP72ZNS","trackingId":"Instagram"},
+  "/nekteck-100w-usb-c-charger-compact-and-foldable-fast-charging-gan-iv-charger-with-6-6-ft-cable-yt-shorts": {"asin":"B0D78Q2KWR","trackingId":"YT-Shorts"}
 };
 
 export default {
