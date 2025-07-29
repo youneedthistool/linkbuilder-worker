d74743c7417c925bd9928494e667c708b8c4571e
youneedthistool <youneedthistools@gmail.com>
2025-07-29 09:17:05 -0300

    Update index.js via planilha

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const redirects = {
-  "/amazon-basics-mesh-pen-holder-and-organizer,-black,": "https://www.amazon.com/dp/B08VP72ZNS?tag=youtube0178d-20"
+  "/mesh-pen-holder-and-organizer,-black,": "https://www.amazon.com/dp/B08VP72ZNS?tag=youtube0178d-20"
 };
 
 export default {
