b1a5bc01d250662a4f46836a8e9adea5fb9fc1ff
youneedthistool <youneedthistools@gmail.com>
2025-07-29 15:26:37 -0300

    Update redirects with ASIN + Tracking ID

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redirects = {
   "/nekteck-100w-usb-c-charger-compact-and-foldable-fast-charging-gan-iv-charger-with-6-6-ft-cable-compatible-with-macbook-air-pro-ipad-pro-and-all-usbc-devices-black-tiktok": {"asin":"B0D78Q2KWR","trackingId":"tiktok01095-20"},
-  "/100w-universal-usb-c-power-adapter-fast-charger-x": {"asin":"B0BY8VJ4MS","trackingId":"x2020-20"}
+  "/100w-universal-usb-c-power-adapter-fast-charger-x": {"asin":"B0BY8VJ4MS","trackingId":"x2020-20"},
+  "/steno-pads-spiral-6x9-12-pack-assorted-bright-colors-yt-shorts": {"asin":"B0BZQPL755","trackingId":"ytshorts010c-20"}
 };
 
 export default {
