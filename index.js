631aabb70b72cf41af5720f1fd2abf8c46d7bf67
youneedthistool <youneedthistools@gmail.com>
2025-07-28 23:51:14 -0300

    Update index.js via planilha

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+
+const redirects = {
+  "/oi": "https://www.amazon.com/dp/B0D1YD7R2C?tag=youtube0178d-20",
+  "/ola-": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta0178d-205",
+  "/oudri": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta0178d-201",
+  "/nekteck-100w-usb-c-charger": "https://www.amazon.com/dp/B0D78Q2KWR?tag=x2020-20"
+};
+
 export default {
   async fetch(request, env, ctx) {
-    const sheetApi = "https://script.google.com/macros/s/AKfycbzZfa08RcxuZFqMgmzIH-s3HPiI4499S7Qi7-Vs5S4hNWFTPOqx1UkBrJ2-fOuBHTs/exec";
-    
-    try {
-      const res = await fetch(sheetApi);
-      const redirects = await res.json();
-
-      const url = new URL(request.url);
-      const path = url.pathname;
-
-      if (redirects[path]) {
-        return Response.redirect(redirects[path], 301);
-      } else {
-        return new Response("Not found", { status: 404 });
-      }
-
-    } catch (e) {
-      return new Response("Erro ao buscar redirecionamentos.", { status: 500 });
+    const url = new URL(request.url);
+    const target = redirects[url.pathname];
+    if (target) {
+      return Response.redirect(target, 301);
     }
-  }
+    return new Response("Not found", { status: 404 });
+  },
 };
