5ebdd596cbaaea171770ba90d7a1330493171eb7
youneedthistool <youneedthistools@gmail.com>
2025-07-28 23:36:59 -0300

    Update index.js via planilha

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const redirects = {
   "/oi": "https://www.amazon.com/dp/B0D1YD7R2C?tag=youtube0178d-20",
   "/ola-": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta0178d-205",
-  "/oudri": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta0178d-201"
+  "/oudri": "https://www.amazon.com/dp/B0D1YD7R2C?tag=insta0178d-201",
+  "/nekteck-100w-usb-c-charger": "https://www.amazon.com/dp/B0D78Q2KWR?tag="
 };
 
 export default {
