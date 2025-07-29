1a52d1b23add097a488f2db63193bd38d7f528eb
youneedthistool <youneedthistools@gmail.com>
2025-07-29 10:40:09 -0300

    Update redirects via Google Sheets

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const redirects = {
-  "/mesh-pen-holder-and-organizer-black": "https://www.amazon.com/Amazon-Basics-Mesh-Pen-Holder/dp/B08VP72ZNS?pd_rd_i=B08VP72ZNS&pd_rd_w=gwG7h&content-id=amzn1.sym.57b80066-10e8-4be7-a5f2-ce3f1faa4959&pf_rd_p=57b80066-10e8-4be7-a5f2-ce3f1faa4959&pf_rd_r=4EPPKJA0RRJF57TECT09&pd_rd_wg=JhqMl&pd_rd_r=242f43b0-784c-4de2-94c7-0ba885110376&th=1&linkCode=ll1&tag=youtube0178d-20&linkId=5dec63b4242126d8b01c48f00f889114&language=en_US&ref_=as_li_ss_tl",
-  "/basics-collapsible-fabric-storage-cubes": "https://www.amazon.com/Amazon-Basics-Collapsible-Storage-Organizer/dp/B071225BBS?pd_rd_w=3tCuA&content-id=amzn1.sym.15aac070-d8be-4c31-b9b5-dbe49c8bb011%3Aamzn1.symc.1065d246-0415-4243-928d-c7025bdd9a27&pf_rd_p=15aac070-d8be-4c31-b9b5-dbe49c8bb011&pf_rd_r=XJ1BT9FTN4XK127K4XHN&pd_rd_wg=AQMDR&pd_rd_r=a050f5bc-fb32-4c75-a926-cb745c4d1a8a&pd_rd_i=B071225BBS&th=1&linkCode=ll1&tag=tiktok01095-20&linkId=0900173f24ef7cadd8dac47215554f5e&language=en_US&ref_=as_li_ss_tl"
+  "/mesh-pen-holder-and-organizer-black": "https://www.amazon.com/Amazon-Basics-Mesh-Pen-Holder/dp/B08VP72ZNS?pd_rd_i=B08VP72ZNS&pd_rd_w=gwG7h&content-id=amzn1.sym.57b80066-10e8-4be7-a5f2-ce3f1faa4959&pf_rd_p=57b80066-10e8-4be7-a5f2-ce3f1faa4959&pf_rd_r=4EPPKJA0RRJF57TECT09&pd_rd_wg=JhqMl&pd_rd_r=242f43b0-784c-4de2-94c7-0ba885110376&th=1&linkCode=ll1&tag=youtube0178d-20&linkId=5dec63b4242126d8b01c48f00f889114&language=en_US&ref_=as_li_ss_tl"
 };
 
 export default {
