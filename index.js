d44181b4487de152fb6059de098a9c8b559bdce6
youneedthistool <youneedthistools@gmail.com>
2025-07-29 15:23:46 -0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const redirects = {
-  "/nekteck-100w-usb-c-charger-compact-and-foldable-fast-charging-gan-iv-charger-with-6-6-ft-cable-compatible-with-macbook-air-pro-ipad-pro-and-all-usbc-devices-black-tiktok": {"asin":"B0D78Q2KWR","trackingId":"tiktok01095-20"},
-  "/100w-universal-usb-c-power-adapter-fast-charger-x": {"asin":"B0BY8VJ4MS","trackingId":"x2020-20"}
+  "/nekteck-100w-usb-c-charger-compact-and-foldable-fast-charging-gan-iv-charger-with-6-6-ft-cable-compatible-with-macbook-air-pro-ipad-pro-and-all-usbc-devices-black-tiktok": {
+    asin: "B0D78Q2KWR",
+    trackingId: "tiktok01095-20"
+  },
+  "/100w-universal-usb-c-power-adapter-fast-charger-x": {
+    asin: "B0BY8VJ4MS",
+    trackingId: "x2020-20"
+  }
 };
 
 export default {
