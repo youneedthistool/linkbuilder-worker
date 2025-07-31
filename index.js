a2d871827266cc5a4e2efbc098b5b35ce31bce8c
youneedthistool <youneedthistools@gmail.com>
2025-07-31 17:00:02 -0300

    Update redirects from Google Sheets

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const redirects = {
-  "/charger-foldable-tiktok01095-20": {"asin":"B0D78Q2KWR","trackingId":"tiktok01095-20"},
-  "/mac-book-pro-charger-100w-usb-c-charger-fast-charger-ytshorts010c-20": {"asin":"B0D59LHFTS","trackingId":"ytshorts010c-20"},
-  "/usb-c-to-usb-c-charging-cable-for-apple-2pack-10ft-tiktok01095-20": {"asin":"B0BJQ93CS9","trackingId":"tiktok01095-20"}
+  "/charger-foldable": {"asin":"B0D78Q2KWR","trackingId":"tiktok01095-20"},
+  "/mac-book-pro-charger-100w-usb-c-charger-fast-charger": {"asin":"B0D59LHFTS","trackingId":"ytshorts010c-20"},
+  "/usb-c-to-usb-c-charging-cable-for-apple-2pack-10ft": {"asin":"B0BJQ93CS9","trackingId":"tiktok01095-20"}
 };
 
 export default {
